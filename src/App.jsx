75c2c84edf860b6d7df28df8532278027d6339ec
mrx_sandy <sandipgadekar143@gmail.com>
2024-08-27 14:39:37 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Dashboard from "./pages/admin/dashboard/Dashboard";
 import MyState from "./context/data/myState";
 import CreateBlog from "./pages/admin/createBlog/CreateBlog";
 import { Toaster } from "react-hot-toast";
-import NoPage from "./pages/nopage/Nopage";
+import NoPage from "./pages/nopage/NoPage";
 
 function App() {
   return (
